b9f8bcd31e272a1c484aaebeb3e17de86d17401c
Cliff Anderson <snappinmambo@gmail.com>
2025-11-27 08:22:10 -0600

    Made an adjustment to the middleware in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import router from './routes/userRoutes.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Application is running fine')
-});
+app.get('/api/user', router);
 
 app.listen(PORT, () => {
   connectDB();
